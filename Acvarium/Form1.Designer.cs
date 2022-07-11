0b7b1e72464fe924aae481bda77222bab60f26bf
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-07-11 11:29:27 +0300

    Добавьте файлы проекта.

---
+++ Acvarium/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Acvarium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Acvarium.Properties.Resources.fish1;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Acvarium.Properties.Resources.acvarium;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Acvarium/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Acvarium
+{
+    public partial class Form1 : Form
+    { 
+        Timer timer = new Timer();
+        bool left_muve;
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            //this.WindowState = FormWindowState.Maximized;
+            //this.FormBorderStyle = FormBorderStyle.None;
+            timer.Interval = 1;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(pictureBox1.Location.X <= Width - 100 && !left_muve)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
+                if (pictureBox1.Location.X >= Width - 110)
+                {
+                    pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                    left_muve = true;
+                }
+            }
+            //else if (pictureBox1.Location.X == Width - 100 || pictureBox1.Location.X == 0)
+            //{
+            //    pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+            //    left_muve = true;
+            //}
+            else if (pictureBox1.Location.X >= 0 && left_muve)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 10, pictureBox1.Location.Y);
+                if (pictureBox1.Location.X <= 10)
+                {
+                    pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                    left_muve = false;
+                }
+            }
+            
+        }
+
+        void fishMove ()
+        {
+            timer.Interval = 500;        }
+    }
+}
